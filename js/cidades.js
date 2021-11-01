4232edde3d1cc5095475a1d05503269ad35f958e
Lukasl1ma <Lukasl1ma@users.noreply.github.com>
2021-11-01 20:58:02 -0300

    update code index.html, homepage.css and cidades.js

--- js/cidades.js
+++ js/cidades.js
@@ -1,25 +1,4 @@
-let dados = [
-    {
-        nome: "Pouso Alegre",
-        totalCasos: 24677,
-        totalObitos: 462,
-        totalRecuperados: 24153,
-        casosAcompanhamento: 62,
-        totalPrimeiraDose: 120626,
-        totalSegundaDose: 83943,
-        totalDoseUnica: 3713
-    },
-    {
-        nome: "Cidade 2",
-        totalCasos: 2,
-        totalObitos: 462,
-        totalRecuperados: 24153,
-        casosAcompanhamento: 62,
-        totalPrimeiraDose: 120626,
-        totalSegundaDose: 83943,
-        totalDoseUnica: 3713
-    }
-]
+let dados
 
 let totalCasos = document.getElementById("totalCasos")
 let casosAtual = document.getElementById("casosAtual")
@@ -27,11 +6,34 @@ let primeiraDose = document.getElementById("primeiraDose")
 let totalVacinados = document.getElementById("totalVacinados")
 let totalMortes = document.getElementById("totalMortes")
 let letalidade = document.getElementById("letalidade")
+let porcentagem1 = document.getElementById("porcentagem1")
+let porcentagem2 = document.getElementById("porcentagem2")
+let porcentagemTexto = document.getElementById("porcentagemTexto")
+let porcentagem2Texto = document.getElementById("porcentagem2Texto")
 const select = document.getElementById("select-cidade")
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
 
 const start = () => {
     changeFront(0)
     select.onchange = handleChange
+}
+
+const preload = () => {
+    readTextFile("../data/cidades.json", function(text){
+        dados = JSON.parse(text);
+        start()
+    });
 }
 
 const handleChange = () => {
@@ -47,6 +49,16 @@ const changeFront = (index) => {
     console.log(dadosCidade)
     totalCasos.innerHTML = dadosCidade.totalCasos
     casosAtual.innerHTML = dadosCidade.casosAcompanhamento
+    primeiraDose.innerHTML = dadosCidade.totalPrimeiraDose
+    totalVacinados.innerHTML = dadosCidade.totalVacinados
+    totalMortes.innerHTML = dadosCidade.totalObitos
+    letalidade.innerHTML = dadosCidade.letalidade
+    porcentagem1.style.width = dadosCidade.p1
+    porcentagemTexto.innerHTML = dadosCidade.p1
+    porcentagem2.style.width = dadosCidade.p2
+    porcentagem2Texto.innerHTML = dadosCidade.p2
 }
 
-start()
+preload()
+
+
