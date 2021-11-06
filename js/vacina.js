66c770dcbc377aaa69336e4b0fb99f5e08b461bf
Lukasl1ma <Lukasl1ma@users.noreply.github.com>
2021-11-06 19:17:00 -0300

    Update code vacinas page

--- js/vacina.js
+++ js/vacina.js
@@ -1,11 +1,15 @@
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
+const tabContentsImg = document.querySelectorAll('[data-tab-content-img]')
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
     tabContents.forEach(tabContent => {
       tabContent.classList.remove('active')
+    })
+    tabContentsImg.forEach(tabContentImg => {
+      tabContentImg.classList.remove('active')
     })
     tabs.forEach(tab => {
       tab.classList.remove('active')
@@ -14,3 +18,4 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
+
